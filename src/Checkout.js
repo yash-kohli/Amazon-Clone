97ec8bc76abd2cc65e320910280263b694e6eb74
yash kohli <yashkohli786@gmail.com>
2020-09-22 21:25:40 +0530

    CheckoutProduct component added which shows added items in basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useDataLayerValue } from "./DataLayer";
 import Subtotal from "./Subtotal";
 function Checkout() {
+  const [{ basket }, dispatch] = useDataLayerValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -10,8 +13,20 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+
         <div className="checkout__basket">
           <h2 className="checkout__title">Your Shopping Basket </h2>
+
+          {/* CheckoutProducta */}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              price={item.price}
+              title={item.title}
+              rating={item.rating}
+              image={item.image}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">
