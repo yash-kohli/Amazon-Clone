b578a43680107066822512cdcd6d5974ca31ddd4
yash kohli <yashkohli786@gmail.com>
2020-09-24 22:23:03 +0530

    user looged in/out display done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useDataLayerValue } from "./DataLayer";
 function App() {
+  const [{}, dispatch] = useDataLayerValue();
+  useEffect(() => {
+    // run once only when app component loads
+
+    // as soon as app loads its listen to changes to login
+    auth.onAuthStateChanged((authUser) => {
+      console.log("USER IS ====>", authUser);
+
+      if (authUser) {
+        // user  logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // user logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      {/* Home */}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+
+        {/* Home */}
+      </div>
+    </Router>
   );
 }
 
